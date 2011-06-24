81851822f6175cbe20b2aed9c46a3ea8e689e9f4
Jacopo Jakuza Romei <jromei@gmail.com>
2011-06-24 11:19:34 +0200

    Failing functional test.

---
+++ src/Avoja/BookMentorBundle/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the Symfony framework.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Avoja\BookMentorBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('AvojaBookMentorBundle:Default:index.html.twig');
+    }
+}
---
+++ src/Avoja/BookMentorBundle/Test/Functional/BasicTest.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace Avoja\BookMentorBundle\Test\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class BasicTest extends WebTestCase
+{
+    public function testSuggestion()
+    {
+        $client = $this->createClient();
+        
+        $crawler = $client->getRequest('GET', 'bookmentor/suggest');
+        
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEquals(1, $crawler->filter('h1:contains("BookMentor")')->count());
+        $this->assertEquals(0, $crawler->filter('.suggestion')->count());
+    }
+}
