0249b7840ba2695e85410df7c2c830e93d613c09
Jacopo Jakuza Romei <jromei@gmail.com>
2011-10-24 09:54:41 +0200

    Functional tests working, template complete, suggestions in controller.

--- src/Avoja/BookMentorBundle/Controller/SuggestionController.php
+++ src/Avoja/BookMentorBundle/Controller/SuggestionController.php
@@ -8,12 +8,17 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class SuggestionController extends Controller 
 {
     /**
-     * 
+     * @Route("/suggestions/{username}")
      */
     public function indexAction()
     {
+        $suggestions = array(
+            'Pro PHP Refactoring',
+            'Cinderella',
+            'Matchu Pitchu',
+        );
         
-        return $this->render('AvojaBookMentorBundle:Suggestion:index.html.twig');
+        return $this->render('AvojaBookMentorBundle:Suggestion:index.html.twig', array('suggestions' => $suggestions));
     }
     
 }
