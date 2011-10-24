1f7ad75bf44d4f5044fd8d28d0b40a349ae15ad5
Jacopo Jakuza Romei <jromei@gmail.com>
2011-10-24 10:35:37 +0200

    Algoritmo di matching fatto.

--- src/Avoja/BookMentorBundle/Mentor.php
+++ src/Avoja/BookMentorBundle/Mentor.php
@@ -4,14 +4,36 @@ namespace Avoja\BookMentorBundle;
 
 class Mentor 
 {
+    private function match($books, $keywords)
+    {
+        $suggestions = array();
+        foreach ($keywords as $keyword)
+        {
+            foreach ($books as $book)
+            {
+                if (false !== strpos(strtolower($book), strtolower($keyword)))
+                {
+                    $suggestions[] = $book;
+                }
+            }
+        }
+        
+        return array_unique($suggestions);
+    }
+    
     public function suggestFor($username)
     {
-        $suggestions = array(
+        $books = array(
             'Pingo pingo',
+            'Jumabo',
+            'The Lord of the Rings',
             'Pro PHP Refactoring',
+            'Casanova',
             'Cinderella',
         );
         
-        return $suggestions;
+        $twitter_keywords = array('refactoring', 'mountain', 'sail', 'cinderella', 'pingo');
+        
+        return $this->match($books, $twitter_keywords);
     }
 }
